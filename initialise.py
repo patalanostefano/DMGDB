1ea242dab8ddddc5a48f4a9c117f8b1cf35fbfc4
patalanostefano <136838737+patalanostefano@users.noreply.github.com>
2024-12-11 22:13:52 +0100

    initial commit

--- initialise.py
+++ initialise.py
@@ -4,7 +4,7 @@ neo4j_config = {
     'url': 'neo4j+ssc://bc1e22a5.databases.neo4j.io',
     'database': 'neo4j',
     'user': 'neo4j',
-    'password': 'FgT82ozrijBRXCmi1oQNK5Le8DAHqz90-YYtHXLjLjk'
+    'password': '-'
 }
 
 initialise_schema(neo4j_config=neo4j_config)
