c3407bd2fc8268660de900600e885cbf74eb2483
patalanostefano <136838737+patalanostefano@users.noreply.github.com>
2024-12-11 22:13:14 +0100

    initial commit

--- ingest_doc.py
+++ ingest_doc.py
@@ -357,7 +357,7 @@ if __name__ == "__main__":
         'url': 'neo4j+ssc://bc1e22a5.databases.neo4j.io',
         'database': 'neo4j',
         'user': 'neo4j',
-        'password': 'FgT82ozrijBRXCmi1oQNK5Le8DAHqz90-YYtHXLjLjk'
+        'password': '-'
     }
     #load_docs(neo4j_config)
     #load_kb(neo4j_config)
